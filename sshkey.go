4b1ff3c7233c1f26690a684a73d358544d094ad0
Mykola Marzhan <mykola.marzhan@percona.com>
2017-08-14 14:30:03 +0300

    BLD-779 don't change selinux context

--- sshkey.go
+++ sshkey.go
@@ -3,8 +3,6 @@ package main
 import (
 	"encoding/json"
 	"net/http"
-
-	selinux "github.com/opencontainers/selinux/go-selinux"
 )
 
 func getSSHKeyHandler(w http.ResponseWriter, req *http.Request) {
@@ -18,15 +16,11 @@ func getSSHKeyHandler(w http.ResponseWriter, req *http.Request) {
 
 func setSSHKeyHandler(w http.ResponseWriter, req *http.Request) {
 	parsedSSHKey, result, err := SSHKey.Write(req.Body)
-	if result != "success" {
+	if result == "success" {
+		w.Header().Set("Location", req.URL.String())
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(parsedSSHKey) // nolint: errcheck
+	} else {
 		returnError(w, req, http.StatusInternalServerError, result, err)
 	}
-
-	if err = selinux.SetFileLabel(SSHKey.KeyPath, "system_u:object_r:httpd_sys_content_t:s0"); err != nil { // nolint
-		returnError(w, req, http.StatusInternalServerError, "Cannot set SELinux context", err)
-	}
-
-	w.Header().Set("Location", req.URL.String())
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(parsedSSHKey) // nolint: errcheck
 }
